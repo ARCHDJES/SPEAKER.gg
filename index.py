0a29c663e0e4e5c1940ebfd83d604f34e3598cc9
ARCHDJES <rhgs130@gmail.com>
2026-09-11 07:57:29 -0300

    update index.py

--- index.py
+++ index.py
@@ -3,9 +3,12 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
+token = os.getenv("DISCORD_TOKEN")
 
 client = discord.Client(intents=discord.Intents.default())
 
 @client.event
 async def on_ready():
   print("Speaker está pronto para falar")
+  
+client.run(token)
